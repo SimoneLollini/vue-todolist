c0f1343857bd7372faeaeeeddc5ed003e1501ce7
SimoneLollini <lollinisimone@gmail.com>
2022-10-28 15:58:45 +0200

    obj can be added to tasks

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,45 @@
+// Rifare l'esercizio della to do list. Questa volta però ogni todo sarà un oggetto, formato da due proprietà:
+// text, una stringa che indica il testo del todo
+// done, un booleano (true/false) che indica se il todo è stato fatto oppure no
+// MILESTONE 1
+// Stampare all'interno di una lista, un item per ogni todo. Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
+// MILESTONE 2
+// Visualizzare a fianco ad ogni item ha una "x": cliccando su di essa, il todo viene rimosso dalla lista.
+// MILESTONE 3
+// Predisporre un campo di input testuale e un pulsante "aggiungi": cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
+let tasks = [
+    // {
+    //     text: 'task di prova',
+    //     done: true
+    // }
+];
 
-import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js'
+
+const { createApp } = Vue
 
 createApp({
     data() {
         return {
-            message: 'Hello Vue!'
+            tasks,
+            newTask: ''
         }
-    }
+    },
+    methods: {
+        // addTask() {
+        //     this.tasks.push(this.newTask)
+        //     console.log(this.tasks);
+        // }
+        addTask() {
+            // console.log(this.tasks);
+            // console.log(this.newTask);
+            // this.tasks.push(this.newTask);
+            this.tasks.push({
+                text: this.newTask,
+                done: false
+            })
+
+        }
+    },
 }).mount('#app')
+
+// console.log(tasks);
